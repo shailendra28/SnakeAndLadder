734e737346eace6abf4f3beb8f2503ea26486d7d
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-03 02:01:44 +0530

    ADDED: Ensure the player exact postion

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,37 +4,44 @@ public class SnakeAndLadder {
 	public static final int SNAKE=2;
     public static void main(String[] args) {
 			int position = 0;
-			System.out.println("Current Position is : " + position);
+			System.out.println(" Current Position is : " + position);
 			
 			while(position < 100){
 			int die = (int)Math.floor( ( ( Math.random() * 10 ) % 6) + 1);
 			int choice=(int)Math.floor((Math.random()*10)%3);
-			System.out.println("Number on die is : " + die);
+			
 			
 			
 			    switch(choice){
 			    
 			    case LADDER:
 			    	
-				System.out.println("Current position is : " + die);
+				System.out.println(" Current position is : " + die);
 				position += die;
-				if(position >= 100){
-					System.out.println("Player win the game!!!!! ");
+				if(position > 100){
+					position -= die;
+					int remaining = 100 - position;
+					System.out.println(" Player needs exact " + remaining + " on die to win! ");
 				}
-				else{
+				else if(position == 100){
+					System.out.println(" Player win the game!!!!!! ");
+				}
+				else
+				{
 					
-					System.out.println("Player got ladder, New position is : " + position);
+					System.out.println(" Player got ladder, New position is : "  + position);
 				}
-				break;
+				 break;
 				
 			    case SNAKE:
 			    	
-				    System.out.println("Current position is : " + die);
+				    System.out.println(" Current position is : " + die);
 				position -= die;
-				if(position < 0){
+				if(position < 0)
+				{
 					position = 0;
 				}
-				    System.out.println("Player got the snake, New position is : " + position);
+				    System.out.println(" Player got the snake, New position is : " + position);
 				break;
 				
 			default:
