d02019a2f8852847957544da8e0a516ac067c1b3
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-03 01:03:18 +0530

    ADDED: Die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,7 +4,8 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 			int position = 0;
 			System.out.println("Current Position is : " + position);
-
+			int die = (int)Math.floor( ( ( Math.random() * 10 ) % 6) + 1);
+			System.out.println("Number on die is : " + die);
 	}
 
 }
