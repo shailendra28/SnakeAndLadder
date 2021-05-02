2a88e557479b0a7aac106be0dcff92c5ba761d9c
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-03 00:55:42 +0530

    ADDED: Current Position Of Player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,7 +3,7 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 			int position = 0;
-			System.out.println("Current Position is:" + position);
+			System.out.println("Current Position is : " + position);
 
 	}
 
